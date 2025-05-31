6ca04ba515c13eeffc7025292b6e4ef58f4460b5
unknown <reshh44@gmail.com>
2025-05-31 20:54:22 +0530

    apis

--- routes/api.php
+++ routes/api.php
@@ -27,5 +27,7 @@ Route::post('login', [AuthController::class, 'login']);
 Route::group(['middleware' => ['auth:api']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/products/search/{title}', [ProductController::class, 'search']);
+    
+    Route::post('/products/store', [ProductController::class, 'store']);
 });
-Route::apiResource('products', ProductController::class)->middleware('auth:api');
+//Route::apiResource('products', ProductController::class)->middleware('auth:api');
